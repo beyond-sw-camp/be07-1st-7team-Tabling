b2a92499a277d104c877a679b83163acc9a53532
sseho <kindseho@naver.com>
2024-06-03 12:31:33 +0900

    store_image 생성문

---
+++ SQL/Insert/store_image.sql
@@ -0,0 +1,17 @@
+
+insert into store_image(store_id,image_path)
+values
+(1,'https://www.ktsketch.co.kr/news/photo/202011/6374_29521_5453.jpg'),
+(2,'https://www.gyeongju.go.kr/upload/content/thumb/20200227/E3F01A2D0EF745FB85AB015EA905A7C4.jpg'),
+(3,'https://www.fitpetmall.com/wp-content/uploads/2023/09/shutterstock_2205178589-1-1.png'),
+(4,'https://www.fitpetmall.com/wp-content/uploads/2023/09/shutterstock_2205178589-1-1.png'),
+(5,'https://www.fitpetmall.com/wp-content/uploads/2023/09/shutterstock_2205178589-1-1.png'),
+(6,'https://www.fitpetmall.com/wp-content/uploads/2023/09/shutterstock_2205178589-1-1.png'),
+(7,'https://www.fitpetmall.com/wp-content/uploads/2023/09/shutterstock_2205178589-1-1.png'),
+(8,'https://www.fitpetmall.com/wp-content/uploads/2023/09/shutterstock_2205178589-1-1.png'),
+(9,'https://www.fitpetmall.com/wp-content/uploads/2023/09/shutterstock_2205178589-1-1.png'),
+(10,'https://www.fitpetmall.com/wp-content/uploads/2023/09/shutterstock_2205178589-1-1.png'),
+(11,'https://www.fitpetmall.com/wp-content/uploads/2023/09/shutterstock_2205178589-1-1.png'),
+(12,'https://www.fitpetmall.com/wp-content/uploads/2023/09/shutterstock_2205178589-1-1.png'),
+(13,'https://www.fitpetmall.com/wp-content/uploads/2023/09/shutterstock_2205178589-1-1.png'),
+;
